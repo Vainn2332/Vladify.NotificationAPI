ac7f0729d32b3a1b801b01301ab2cbc629f27dbf
Vladislav Pomozov <vladuk2332@gmail.com>
2026-03-29 20:24:12 +0300

    added user entity

---
+++ Vladify.DataAccess/Entities/User.cs
@@ -0,0 +1,11 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace Vladify.DataAccess.Entities;
+
+public class User
+{
+    [BsonId]
+    public int Id { get; set; }
+
+    public required NotificationSubscription NotificationSubscription { get; set; }
+}
