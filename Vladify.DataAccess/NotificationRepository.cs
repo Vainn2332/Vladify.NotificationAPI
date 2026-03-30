bba0123518587464b3698d314647f871fbf5ea3f
Vladislav Pomozov <vladuk2332@gmail.com>
2026-03-30 16:51:02 +0300

    add base repository

---
+++ Vladify.DataAccess/NotificationRepository.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Driver;
+using Vladify.DataAccess.Entities;
+
+namespace Vladify.DataAccess;
+
+public class NotificationRepository
+{
+    private readonly IMongoCollection<NotificationInfo> _notifications;
+
+    public NotificationRepository(IMongoCollection<NotificationInfo> notifications)
+    {
+        _notifications = notifications;
+    }
+
+
+}
