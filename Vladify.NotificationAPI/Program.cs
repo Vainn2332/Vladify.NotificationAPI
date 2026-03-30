11acd18e35ed0820548c17bad742a6458d11538d
Vladislav Pomozov <vladuk2332@gmail.com>
2026-03-30 17:50:38 +0300

    it works(ibut user secrets not somewhy)

--- Vladify.NotificationAPI/Program.cs
+++ Vladify.NotificationAPI/Program.cs
@@ -1,14 +1,20 @@
+using Scalar.AspNetCore;
+using Vladify.DataAccess.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
 builder.Services.AddOpenApi();
 
+builder.Services.AddMongoDb(builder.Configuration);
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
---
+++ Vladify.NotificationAPI/Test.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Vladify.DataAccess;
+using Vladify.DataAccess.Entities;
+
+
+namespace Vladify.NotificationAPI;
+
+[Route("api/[controller]")]
+[ApiController]
+public class Test(INotificationRepository _repo) : ControllerBase
+{
+
+
+    [HttpGet]
+    public IEnumerable<string> Get()
+    {
+        return new string[] { "value1", "value2" };
+    }
+
+    [HttpGet("{id}")]
+    public Task<NotificationInfo> Get(string id, CancellationToken cancellationToken = default)
+    {
+        return _repo.GetByIdAsync(id, cancellationToken)!;
+    }
+
+    [HttpPost]
+    public Task<NotificationInfo> Post([FromBody] NotificationInfo notification, CancellationToken cancellationToken = default)
+    {
+        return _repo.CreateAsync(notification, cancellationToken);
+    }
+
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody] string value)
+    {
+    }
+
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+    }
+}
