e25f7df662e1af2816cf4d13db90cb729d55aaea
Vladislav Pomozov <vladuk2332@gmail.com>
2026-03-30 12:59:00 +0300

    added mongoDbOptions

---
+++ Vladify.BuisnessLogic/Options/MongoDbOptions.cs
@@ -0,0 +1,12 @@
+﻿namespace Vladify.BuisnessLogic.Options;
+
+public class MongoDbOptions
+{
+    const string SectionName = "MongoDb";
+
+    public required string ConnectionString { get; set; }
+
+    public required string DbName { get; set; }
+
+    public required string CollectionName { get; set; }
+}
