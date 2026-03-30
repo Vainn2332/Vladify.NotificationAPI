7d6b86457bd5673c7ef77dde25273354358e1ab1
Vladislav Pomozov <vladuk2332@gmail.com>
2026-03-30 15:46:19 +0300

    add DAL Extensions

---
+++ Vladify.DataAccess/Extensions/DataAccessExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MongoDB.Driver;
+
+namespace Vladify.DataAccess.Extensions;
+
+public static class DataAccessExtensions
+{
+    public static IServiceCollection AddServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddSingleton<IMongoClient>(serviceProvider =>
+        {
+            var connectionString = configuration.GetConnectionString("MongoDb")
+                ?? throw new InvalidOperationException($"Connection string 'MongoDb' not found!");
+
+            return new MongoClient(connectionString);
+        });
+
+        return services;
+    }
+}
