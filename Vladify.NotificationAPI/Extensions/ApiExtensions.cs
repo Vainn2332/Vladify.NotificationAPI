d870ff3292aa969723eb734884472ef3a3f2b966
Vladislav Pomozov <vladuk2332@gmail.com>
2026-03-30 13:27:26 +0300

    configure MongoOptions

---
+++ Vladify.NotificationAPI/Extensions/ApiExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Vladify.BuisnessLogic.Options;
+
+namespace Vladify.NotificationAPI.Extensions;
+
+public static class ApiExtensions
+{
+    public static IServiceCollection ConfigureOptions(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.Configure<MongoDbOptions>(configuration.GetSection(MongoDbOptions.SectionName));
+
+        return services;
+    }
+}
