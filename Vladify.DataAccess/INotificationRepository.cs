67217be82c469baff85ed0c901492370f5bce058
Vladislav Pomozov <vladuk2332@gmail.com>
2026-03-30 16:50:47 +0300

    add repositoryInterface

---
+++ Vladify.DataAccess/INotificationRepository.cs
@@ -0,0 +1,12 @@
+﻿using Vladify.DataAccess.Entities;
+
+namespace Vladify.DataAccess;
+
+public interface INotificationRepository
+{
+    public Task<NotificationInfo> CreateAsync(NotificationInfo notification);
+    public Task<NotificationInfo?> GetByIdAsync(string id);
+    public Task<List<NotificationInfo>> GetAllAsync(string pageNumber, string pageSize);
+    public Task UpdateAsync(NotificationInfo notification);
+    public Task DeleteAsync(string id);
+}
