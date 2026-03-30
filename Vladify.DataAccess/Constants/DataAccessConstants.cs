f7bc7cef1f3094f35a9596ff5691d9322fbd4483
Vladislav Pomozov <vladuk2332@gmail.com>
2026-03-30 16:49:17 +0300

    add constants

---
+++ Vladify.DataAccess/Constants/DataAccessConstants.cs
@@ -0,0 +1,10 @@
+﻿namespace Vladify.DataAccess.Constants;
+
+public static class DataAccessConstants
+{
+    public const string DatabaseName = "MongoDb";
+
+    public const string CollectionName = "Notifications";
+
+
+}
