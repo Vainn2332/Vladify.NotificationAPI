8666a8a3cc14cb8caaf2d84ef0dea0ad8afa44a3
Vladislav Pomozov <vladuk2332@gmail.com>
2026-04-02 14:07:26 +0300

    add BLL extensions

---
+++ Vladify.BuisnessLogic/Extensions/BusinessLogicExtensions.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.DependencyInjection;
+using Vladify.BuisnessLogic.Interfaces;
+using Vladify.BuisnessLogic.MapperProfiles;
+
+namespace Vladify.BuisnessLogic.Extensions;
+
+public static class BusinessLogicExtensions
+{
+    public static IServiceCollection AddBusinessLogicLayer(this IServiceCollection services)
+    {
+        services
+            .AddServices()
+            .AddMapping();
+
+        return services;
+    }
+
+    public static IServiceCollection AddServices(this IServiceCollection services)
+    {
+        services.AddScoped<INotificationService, NotificationService>();
+
+        return services;
+    }
+
+    public static IServiceCollection AddMapping(this IServiceCollection services)
+    {
+        services.AddAutoMapper(cfg => { }, typeof(NotificationProfile).Assembly);
+
+        var serviceProvider = services.BuildServiceProvider();
+        var mapper = serviceProvider.GetRequiredService<IMapper>();
+
+        mapper.ConfigurationProvider.AssertConfigurationIsValid();
+
+        return services;
+    }
+}
