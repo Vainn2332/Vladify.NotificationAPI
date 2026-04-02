acd959ac1de84f6d1e929773a6d342271ea96cfb
Vladislav Pomozov <vladuk2332@gmail.com>
2026-04-02 16:19:51 +0300

    change userId type to Guid

--- Vladify.DataAccess/Entities/NotificationInfo.cs
+++ Vladify.DataAccess/Entities/NotificationInfo.cs
@@ -9,7 +9,8 @@ public class NotificationInfo
     [BsonRepresentation(BsonType.ObjectId)]
     public required string Id { get; set; }
 
-    public required string UserId { get; set; }
+    [BsonGuidRepresentation(GuidRepresentation.Standard)]
+    public required Guid UserId { get; set; }
 
     public required string EmailAddress { get; set; }
 
