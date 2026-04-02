c003b7551ac2cadb344359a4ee44f23dda604cce
Vladislav Pomozov <vladuk2332@gmail.com>
2026-04-02 11:57:39 +0300

    add models

---
+++ Vladify.BuisnessLogic/Models/NotificationModel.cs
@@ -0,0 +1,16 @@
+﻿using Vladify.DataAccess.Entities;
+
+namespace Vladify.BuisnessLogic.Models;
+
+public class NotificationModel
+{
+    public required string Id { get; set; }
+
+    public required string UserId { get; set; }
+
+    public required string EmailAddress { get; set; }
+
+    public string? PhoneNumber { get; set; }
+
+    public required NotificationSubscription NotificationSubscription { get; set; }
+}
---
+++ Vladify.BuisnessLogic/Models/NotificationRequestModel.cs
@@ -0,0 +1,14 @@
+﻿using Vladify.DataAccess.Entities;
+
+namespace Vladify.BuisnessLogic.Models;
+
+public class NotificationRequestModel
+{
+    public required string UserId { get; set; }
+
+    public required string EmailAddress { get; set; }
+
+    public string? PhoneNumber { get; set; }
+
+    public required NotificationSubscription NotificationSubscription { get; set; }
+}
