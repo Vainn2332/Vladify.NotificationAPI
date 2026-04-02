01cb79d10675bfc9a969a0ed375b56b687c12730
Vladislav Pomozov <vladuk2332@gmail.com>
2026-04-02 20:49:31 +0300

    minor fixes

--- Vladify.DataAccess/Options/MongoDbOptions.cs
+++ Vladify.DataAccess/Options/MongoDbOptions.cs
@@ -4,7 +4,7 @@ public class MongoDbOptions
 {
     public const string SectionName = "MongoDbOptions";
 
-    public required string DbName { get; set; }
+    public required string DatabaseName { get; set; }
 
     public required string ConnectionString { get; set; }
 }
