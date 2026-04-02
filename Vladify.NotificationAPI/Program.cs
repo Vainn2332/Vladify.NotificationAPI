f74594174b47332d57ad90618a7055df2cf5ab4a
Vladislav Pomozov <vladuk2332@gmail.com>
2026-04-02 14:14:58 +0300

    add BLL in program

--- Vladify.NotificationAPI/Program.cs
+++ Vladify.NotificationAPI/Program.cs
@@ -1,8 +1,12 @@
+using Vladify.BuisnessLogic.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
 builder.Services.AddOpenApi();
+
+builder.Services.AddBusinessLogicLayer(builder.Configuration);
 
 var app = builder.Build();
 
