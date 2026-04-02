d34d61c3100542a0dc9f2621625caa9553249412
Vladislav Pomozov <vladuk2332@gmail.com>
2026-04-02 13:15:52 +0300

    add mappingProfile

---
+++ Vladify.BuisnessLogic/MapperProfiles/NotificationProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Vladify.BuisnessLogic.Models;
+using Vladify.DataAccess.Entities;
+
+namespace Vladify.BuisnessLogic.MapperProfiles;
+
+public class NotificationProfile : Profile
+{
+    public NotificationProfile()
+    {
+        CreateMap<NotificationModel, NotificationInfo>().ReverseMap();
+
+        CreateMap<NotificationRequestModel, NotificationModel>()
+            .ForMember(dest => dest.Id, opt => opt.Ignore());
+
+        CreateMap<NotificationRequestModel, NotificationInfo>()
+           .ForMember(dest => dest.Id, opt => opt.Ignore());
+
+    }
+}
