b11ea69684539923441ccb172fd0a3850c74e53b
Vladislav Pomozov <vladuk2332@gmail.com>
2026-04-02 12:00:32 +0300

    add interface

---
+++ Vladify.BuisnessLogic/Interfaces/INotificationService.cs
@@ -0,0 +1,13 @@
+﻿
+using Vladify.BuisnessLogic.Models;
+
+namespace Vladify.BuisnessLogic.Interfaces;
+
+public interface INotificationService
+{
+    public Task<NotificationModel> CreateAsync(NotificationModel notificationModel, CancellationToken cancellationToken);
+    public Task<NotificationModel?> GetByIdAsync(string id, CancellationToken cancellationToken);
+    public Task<IEnumerable<NotificationModel>> GetAllAsync(int pageNumber, int pageSize, CancellationToken cancellationToken);
+    public Task<NotificationModel> UpdateAsync(NotificationModel notificationModel, CancellationToken cancellationToken);
+    public Task DeleteAsync(string id, CancellationToken cancellationToken);
+}
