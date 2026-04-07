e04fe064059cc8f7f7783dc59cbe73f6e873d1aa
Vladislav Pomozov <vladuk2332@gmail.com>
2026-04-07 13:51:09 +0300

    Feature/add mongo service (#6)
    * added user entity
    
    * updated userEntity
    
    * add mongoConfiguration in appsettings
    
    * add mongoDbOptions
    
    * configure MongoOptions
    
    * add notFoundException
    
    * add mongoCLient
    
    * register extensions in program
    
    * add subscription field
    
    * Revert "add subscription field"
    
    This reverts commit caf7a57128c5ac77f086d70aaebab90b62024a7b.
    
    * update user Entity
    
    * rename user Entity
    
    * Revert "add mongoConfiguration in appsettings"
    
    This reverts commit a9fe6d4569c59ef8ee3ce1ad5f20005156506b17.
    
    * Revert "add mongoDbOptions"
    
    This reverts commit 5c1797974a05bb20ede9ec50a424a938b66ffee3.
    
    * Revert "register extensions in program"
    
    This reverts commit 5b2ee641130e1c3a9ce3e559856db35466926eb2.
    
    * delete apiExtensions
    
    * update appsettings
    
    * rename userEntity
    
    * Revert "add notFoundException"
    
    This reverts commit 36df520f9e481f9e8a2b2219b201a6f89eef1c70.
    
    * add DAL Extensions
    
    * change name
    
    * update extensions
    
    * add constants
    
    * add repositoryInterface
    
    * add base repository
    
    * change Id to string in entity
    
    * add cancellation token to interface
    
    * update NotificationRepo
    
    * it works(ibut user secrets not somewhy)
    
    * fix
    
    * fix issue when Id didn t map
    
    * add repo to DI
    
    * add extension method 'addDal'
    
    * delete white spaces
    
    * minor fixes
    
    * fix issues
    
    * add models
    
    * add interface
    
    * add getByUserId method
    
    * add notFoundException
    
    * edit interface
    
    * add service
    
    * add mappingProfile
    
    * add BLL extensions
    
    * update extensions
    
    * add BLL in program
    
    * edit model
    
    * edit mapping
    
    * update endpoints
    
    * add scalar
    
    * change userId type to Guid
    
    * change userId type to Guid
    
    * add mongoDbOptions
    
    * update appsettings
    
    * minor fixes
    
    * update extensions
    
    * minor fix
    
    * add some tests
    
    * update ci
    
    * add reportPath
    
    * add more tests
    
    * update tests
    
    * minor fix
    
    * delete test controller
    
    * remove dead code test
    
    * minor fix
    
    * minor fix

---
+++ Vladify.BuisnessLogic/MapperProfiles/NotificationProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Vladify.BuisnessLogic.Models;
+using Vladify.DataAccess.Entities;
+
+namespace Vladify.BuisnessLogic.MapperProfiles;
+
+public class NotificationProfile : Profile
+{
+    public NotificationProfile()
+    {
+        CreateMap<NotificationModel, NotificationInfo>().ReverseMap();
+
+        CreateMap<NotificationSubscriptionModel, NotificationSubscription>().ReverseMap();
+
+        CreateMap<NotificationRequestModel, NotificationInfo>()
+           .ForMember(dest => dest.Id, opt => opt.Ignore());
+    }
+}
