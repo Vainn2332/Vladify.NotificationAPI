15863a9c01f8d50957cb3b6c43c2c228c8125556
Vladislav Pomozov <vladuk2332@gmail.com>
2026-04-13 16:52:34 +0300

    add interface

---
+++ Vladify.BuisnessLogic/Interfaces/IEmailService.cs
@@ -0,0 +1,6 @@
+﻿namespace Vladify.BuisnessLogic.Interfaces;
+
+public interface IEmailService
+{
+    public Task SendAsync(string receiver, string subject, string message, CancellationToken cancellationToken);
+}
