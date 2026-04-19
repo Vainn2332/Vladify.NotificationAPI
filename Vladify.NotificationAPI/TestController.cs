a06d006295919bc2ea2a45416f2142607358dd03
Vladislav Pomozov <vladuk2332@gmail.com>
2026-04-19 16:12:38 +0300

    add testController

---
+++ Vladify.NotificationAPI/TestController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Vladify.BuisnessLogic;
+using Vladify.BuisnessLogic.Interfaces;
+using Vladify.BuisnessLogic.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Vladify.NotificationAPI
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController(TESTProducer producer, INotificationService _notificationService) : ControllerBase
+    {
+
+        [HttpPost("/testKafka")]
+        public void Post()
+        {
+            producer.CreateMessageAsync();
+        }
+
+        [HttpPost]
+        public Task<UserNotificationSettingsModel> Post([FromBody] UserNotificationSettingsRequestModel userNotificationSettingsRequestModel, CancellationToken cancellationToken = default)
+        {
+            return _notificationService.CreateAsync(userNotificationSettingsRequestModel, cancellationToken);
+        }
+
+        [HttpGet]
+        public Task<IEnumerable<UserNotificationSettingsModel>> Get(int pageNumber, int pageSize, CancellationToken cancellationToken)
+        {
+            return _notificationService.GetAllAsync(pageNumber, pageSize, cancellationToken);
+        }
+    }
+}
