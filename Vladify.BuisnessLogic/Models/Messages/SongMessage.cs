7c618da18240916830e10d5fecd3f9ca180a189a
Vladislav Pomozov <vladuk2332@gmail.com>
2026-04-19 11:13:15 +0300

    add songMessage DTO

---
+++ Vladify.BuisnessLogic/Models/Messages/SongMessage.cs
@@ -0,0 +1,10 @@
+﻿namespace Vladify.BuisnessLogic.Models.Messages;
+
+public class SongMessage
+{
+    public required string Title { get; set; }
+
+    public required string Album { get; set; }
+
+    public required string Author { get; set; }
+}
