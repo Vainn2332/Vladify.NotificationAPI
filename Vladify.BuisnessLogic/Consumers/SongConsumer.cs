1aac8675a71a1cd88ab9eff2816322ef9de2f9ab
Vladislav Pomozov <vladuk2332@gmail.com>
2026-04-19 11:13:21 +0300

    add consumer

---
+++ Vladify.BuisnessLogic/Consumers/SongConsumer.cs
@@ -0,0 +1,26 @@
+﻿using MassTransit;
+using Vladify.BuisnessLogic.Interfaces;
+using Vladify.BuisnessLogic.Models.Messages;
+
+namespace Vladify.BuisnessLogic.Consumers;
+
+public class SongConsumer : IConsumer<SongMessage>
+{
+    private readonly IEmailService _emailService;
+
+    public SongConsumer(IEmailService emailService)
+    {
+        _emailService = emailService;
+    }
+
+
+    public Task Consume(ConsumeContext<SongMessage> context)
+    {
+        var songData = context.Message;
+        var message =
+            @$"<p>У {songData.Author} в альбоме {songData.Album} вышел новый трек {songData.Title}! Успей заценить</p>";
+        var subject = "Новый трек";
+
+        return _emailService.SendToAllUsersAsync(subject, message, CancellationToken.None);
+    }
+}
