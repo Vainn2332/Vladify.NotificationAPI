866075e7b9b07b5b7222149e4fd89752e4a5d1c4
Vladislav Pomozov <vladuk2332@gmail.com>
2026-04-20 10:44:19 +0300

    add producer

---
+++ Vladify.BuisnessLogic/Interfaces/IKafkaProducerService.cs
@@ -0,0 +1,6 @@
+﻿namespace Vladify.BuisnessLogic.Interfaces;
+
+public interface IKafkaProducerService
+{
+    public Task SendMessageAsync<TMessage>(string topic, TMessage message, CancellationToken cancellationToken, string? key = null);
+}
---
+++ Vladify.BuisnessLogic/KafkaProducerService.cs
@@ -0,0 +1,35 @@
+﻿using Confluent.Kafka;
+using Microsoft.Extensions.Options;
+using System.Text.Json;
+using Vladify.BuisnessLogic.Interfaces;
+using Vladify.BuisnessLogic.Options;
+
+namespace Vladify.BuisnessLogic;
+
+public class KafkaProducerService : IKafkaProducerService
+{
+    private IProducer<string, string> _producer;
+    private readonly KafkaConsumerOptions _options;
+
+    public KafkaProducerService(IOptions<KafkaConsumerOptions> options)
+    {
+        _options = options.Value;
+
+        var config = new ProducerConfig()
+        {
+            BootstrapServers = _options.ServerHost
+        };
+
+        _producer = new ProducerBuilder<string, string>(config).Build();
+    }
+    public Task SendMessageAsync<TMessage>(string topic, TMessage message, CancellationToken cancellationToken, string? key = null)
+    {
+        var jsonMessage = JsonSerializer.Serialize(message);
+
+        return _producer.ProduceAsync(topic, new Message<string, string>()
+        {
+            Key = key!,
+            Value = jsonMessage
+        }, cancellationToken);
+    }
+}
