62fb7af327440087b518295df0e61cbdda5eb441
Vladislav Pomozov <vladuk2332@gmail.com>
2026-04-27 14:03:32 +0300

    add message

---
+++ Vladify.BuisnessLogic/Messages/SongCreatedMessage.cs
@@ -0,0 +1,10 @@
+﻿namespace Vladify.BuisnessLogic.Messages;
+
+public class SongCreatedMessage
+{
+    public required string Title { get; set; }
+
+    public required string Album { get; set; }
+
+    public required string Author { get; set; }
+}
