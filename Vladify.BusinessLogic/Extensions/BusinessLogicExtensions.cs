c916c6edf2c9cee8d8e480baec8b3c4e053764e5
Vladislav Pomozov <vladuk2332@gmail.com>
2026-05-03 12:54:15 +0300

    register consumer

--- Vladify.BusinessLogic/Extensions/BusinessLogicExtensions.cs
+++ Vladify.BusinessLogic/Extensions/BusinessLogicExtensions.cs
@@ -63,6 +63,7 @@ public static class BusinessLogicExtensions
         services.AddMassTransit(x =>
         {
             x.AddConsumer<EmailSenderConsumer>();
+            x.AddConsumer<CreateUserNotificationSettingsConsumer>();
 
             x.UsingRabbitMq((context, cfg) =>
             {
