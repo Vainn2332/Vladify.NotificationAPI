78704da011e188b0c564275bfeadb853b6b50af3
Vladislav Pomozov <vladuk2332@gmail.com>
2026-06-19 16:44:33 +0300

    add patch dto

---
+++ Vladify.DataAccess/Dto/PatchSubscriptionDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Vladify.DataAccess.Dto;
+
+public class PatchSubscriptionDto
+{
+    public required string Id { get; set; }
+
+    // there will be other properties related to other notification subscriptions in the future
+    public bool? IsEmailSubscribed { get; set; }
+}
