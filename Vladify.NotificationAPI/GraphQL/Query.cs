9d207913890b3d48461491e743ab842ede5c5b8b
Vladislav Pomozov <vladuk2332@gmail.com>
2026-06-19 16:05:08 +0300

    add Query class

---
+++ Vladify.NotificationAPI/GraphQL/Query.cs
@@ -0,0 +1,22 @@
+﻿using Vladify.BusinessLogic.Interfaces;
+using Vladify.BusinessLogic.Models;
+
+namespace Vladify.NotificationAPI.GraphQL;
+
+public class Query(INotificationService _notificationService)
+{
+    public Task<UserNotificationSettingsModel?> GetNotificationByIdAsync(string id, CancellationToken cancellationToken)
+    {
+        return _notificationService.GetByIdAsync(id, cancellationToken);
+    }
+
+    public Task<List<UserNotificationSettingsModel>> GetNotificationsAsync(int pageNumber, int pageSize, CancellationToken cancellationToken)
+    {
+        return _notificationService.GetAllAsync(pageNumber, pageSize, cancellationToken);
+    }
+
+    public Task<List<UserNotificationSettingsModel>> GetEmailSubscribersAsync(int pageNumber, int pageSize, CancellationToken cancellationToken)
+    {
+        return _notificationService.GetEmailSubscribersAsync(pageNumber, pageSize, cancellationToken);
+    }
+}
