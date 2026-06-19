c7cc93ef18111cedd07213c86c033547e0670f4d
Vladislav Pomozov <vladuk2332@gmail.com>
2026-06-19 16:56:42 +0300

    add mutation

---
+++ Vladify.NotificationAPI/GraphQL/Mutation.cs
@@ -0,0 +1,17 @@
+﻿using Vladify.BusinessLogic.Interfaces;
+using Vladify.BusinessLogic.Models;
+
+namespace Vladify.NotificationAPI.GraphQL;
+
+public class Mutation(INotificationService _notificationService)
+{
+    public Task<UserNotificationSettingsModel> UpdateNotificationSettingsAsync(UserNotificationSettingsModel input, CancellationToken cancellationToken)
+    {
+        return _notificationService.UpdateAsync(input, cancellationToken);
+    }
+
+    public Task PatchSubscriptionAsync(UserNotificationSubscriptionPatchRequestModel input, CancellationToken cancellationToken)
+    {
+        return _notificationService.PatchSubscriptionAsync(input, cancellationToken);
+    }
+}
