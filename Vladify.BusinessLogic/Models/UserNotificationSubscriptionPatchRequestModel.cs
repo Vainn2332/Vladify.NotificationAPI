b2f92f5ab00d58552616e8ce24e97335f86a7e00
Vladislav Pomozov <vladuk2332@gmail.com>
2026-06-19 16:44:33 +0300

    add patch dto

---
+++ Vladify.BusinessLogic/Models/UserNotificationSubscriptionPatchRequestModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Vladify.BusinessLogic.Models;
+
+public class UserNotificationSubscriptionPatchRequestModel
+{
+    public required string Id { get; set; }
+
+    // there will be other properties related to other notification subscriptions in the future
+    public bool? IsEmailSubscribed { get; set; }
+}
