cb67594f181a0612e01e326c38cdb07f803697cf
Vladislav Pomozov <vladuk2332@gmail.com>
2026-06-22 12:03:54 +0300

    register in DI

---
+++ Vladify.NotificationAPI/Extensions/ApiExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Vladify.BusinessLogic.Extensions;
+using Vladify.NotificationAPI.GraphQL;
+
+namespace Vladify.NotificationAPI.Extensions;
+
+public static class ApiExtensions
+{
+    public static IServiceCollection AddAppServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        return services
+            .AddBusinessLogicLayer(configuration)
+            .AddGraphQL();
+    }
+
+    public static IServiceCollection AddGraphQL(this IServiceCollection services)
+    {
+        services
+           .AddGraphQLServer()
+           .AddQueryType<Query>()
+           .AddMutationType<Mutation>();
+
+        return services;
+    }
+}
