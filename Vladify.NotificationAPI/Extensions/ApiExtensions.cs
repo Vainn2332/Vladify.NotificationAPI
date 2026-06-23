0f17ab1a7e8c7e8486b612b3b2122f288a11a058
Vladislav Pomozov <vladuk2332@gmail.com>
2026-06-23 11:28:45 +0300

    add authentication

--- Vladify.NotificationAPI/Extensions/ApiExtensions.cs
+++ Vladify.NotificationAPI/Extensions/ApiExtensions.cs
@@ -1,4 +1,8 @@
-﻿using Vladify.BusinessLogic.Extensions;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Vladify.BusinessLogic.Exceptions;
+using Vladify.BusinessLogic.Extensions;
+using Vladify.BusinessLogic.Options;
 using Vladify.NotificationAPI.Constants;
 using Vladify.NotificationAPI.GraphQL;
 using Vladify.NotificationAPI.GraphQL.Mutations;
@@ -12,7 +16,35 @@ public static class ApiExtensions
     {
         return services
             .AddBusinessLogicLayer(configuration)
+            .AddJwtBasedAuthentication(configuration)
             .AddGraphQL();
+    }
+
+    public static IServiceCollection AddJwtBasedAuthentication(this IServiceCollection services, IConfiguration configuration)
+    {
+        var auth0Options = configuration.GetSection(Auth0Options.SectionName).Get<Auth0Options>()
+            ?? throw new NotFoundException($"Configuration section{Auth0Options.SectionName} not found!");
+
+        var domain = auth0Options.Domain;
+        var audience = auth0Options.Audience;
+
+        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.Authority = $"https://{domain}";
+
+                options.Audience = audience;
+
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true
+                };
+            });
+
+        return services;
     }
 
     public static IServiceCollection AddGraphQL(this IServiceCollection services)
