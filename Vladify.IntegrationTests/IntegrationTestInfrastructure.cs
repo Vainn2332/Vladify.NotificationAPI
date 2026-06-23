0758ae7055b5f415c29ace287684a94205e21dc7
Vladislav Pomozov <vladuk2332@gmail.com>
2026-06-23 14:36:37 +0300

    add integration test infrastructure

---
+++ Vladify.IntegrationTests/IntegrationTestInfrastructure.cs
@@ -0,0 +1,119 @@
+﻿using AutoFixture;
+using MassTransit;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using Moq;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Testcontainers.MongoDb;
+
+namespace Vladify.IntegrationTests;
+
+public class IntegrationTestInfrastructure : IAsyncLifetime
+{
+    private readonly MongoDbContainer _mongoDbContainer = new MongoDbBuilder().Build();
+    private IMongoDatabase _database = null!;
+    private readonly IFixture _fixture = new Fixture();
+    public WebApplicationFactory<Program> Factory { get; private set; } = null!;
+
+    public HttpClient Client { get; private set; } = null!;
+
+    public async Task InitializeAsync()
+    {
+        await _mongoDbContainer.StartAsync();
+
+        var mongoClient = new MongoClient(_mongoDbContainer.GetConnectionString());
+        _database = mongoClient.GetDatabase("TestDb");
+
+        Factory = new WebApplicationFactory<Program>().WithWebHostBuilder(builder =>
+        {
+            builder.ConfigureServices(services =>
+            {
+                ConfigureTestServices(services);
+
+                services.PostConfigure<JwtBearerOptions>(JwtBearerDefaults.AuthenticationScheme, options =>
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateAudience = true,
+                    ValidAudience = TestConstants.Audience,
+                    ValidateIssuer = true,
+                    ValidIssuer = TestConstants.Issuer,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(TestConstants.TestSecretKey))
+                });
+            });
+        });
+
+        Client = Factory.CreateClient();
+    }
+
+    public async Task ResetDataAsync()
+    {
+        var collections = await _database.ListCollectionNamesAsync();
+        var collectionNames = await collections.ToListAsync();
+
+        foreach (var collectionName in collectionNames)
+        {
+            await _database
+                .GetCollection<BsonDocument>(collectionName)
+                .DeleteManyAsync(Builders<BsonDocument>.Filter.Empty);
+        }
+    }
+
+    public string GenerateTestJWT()
+    {
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(TestConstants.TestSecretKey));
+        var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+        var claims = new List<Claim>
+        {
+            new Claim(TestConstants.CustomEmailClaimName,_fixture.Create<string>())
+        };
+
+        var token = new JwtSecurityToken(
+            issuer: TestConstants.Issuer,
+            audience: TestConstants.Audience,
+            claims: claims,
+            expires: DateTime.Now.AddMinutes(5),
+            signingCredentials: credentials
+            );
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+
+    public async Task DisposeAsync()
+    {
+        Client.Dispose();
+        await Factory.DisposeAsync();
+        await _mongoDbContainer.DisposeAsync();
+    }
+
+    public async Task<TEntity> SeedDataAsync<TEntity>(string collectionName, TEntity entity) where TEntity : class
+    {
+        var collection = _database.GetCollection<TEntity>(collectionName);
+        await collection.InsertOneAsync(entity);
+
+        return entity;
+    }
+
+    private static void ConfigureTestServices(IServiceCollection services)
+    {
+        services
+            .RemoveAll<IPublishEndpoint>();
+
+        var publishEndpointMock = new Mock<IPublishEndpoint>();
+        publishEndpointMock
+            .Setup(m => m.Publish(It.IsAny<object>(), It.IsAny<CancellationToken>()))
+            .Returns(Task.CompletedTask);
+
+        services.AddScoped(serviceProvider => publishEndpointMock.Object);
+    }
+}
---
+++ Vladify.IntegrationTests/TestConstants.cs
@@ -0,0 +1,14 @@
+﻿namespace Vladify.IntegrationTests;
+
+public static class TestConstants
+{
+    public const string Audience = "testAudience";
+
+    public const string Issuer = "testIssuer";
+
+    public const string TestSecretKey = "superMegaTestSecretKey";
+
+    public const string BaseClaimNamespace = "https://vladify.com/";
+
+    public const string CustomEmailClaimName = $"{BaseClaimNamespace}email";
+}
