d703abfb4f23099c674bc2da23f96ce5adfe9247
Vladislav Pomozov <vladuk2332@gmail.com>
2026-06-24 13:34:27 +0300

    add base GraphQl response wrapper

---
+++ Vladify.IntegrationTests/GraphQL/GraphQlErrorMessage.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Vladify.IntegrationTests.GraphQL;
+
+public class GraphQlErrorMessage
+{
+    [JsonPropertyName("message")]
+    public required string Message { get; set; }
+}
---
+++ Vladify.IntegrationTests/GraphQL/GraphQlResponse.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Vladify.IntegrationTests.GraphQL;
+
+public class GraphQlResponse<TClass>
+{
+    [JsonPropertyName("data")]
+    public required TClass Data { get; set; }
+
+    [JsonPropertyName("errors")]
+    public required GraphQlErrorMessage[] Errors { get; set; } = [];
+}
