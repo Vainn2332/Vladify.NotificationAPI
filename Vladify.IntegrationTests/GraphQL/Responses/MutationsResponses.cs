1d57c2c43c9c9af7b9dfd0e3ad28d1a1bca63878
Vladislav Pomozov <vladuk2332@gmail.com>
2026-06-24 13:44:02 +0300

    add query and mutation response records

---
+++ Vladify.IntegrationTests/GraphQL/Responses/MutationsResponses.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+using Vladify.BusinessLogic.Models;
+
+namespace Vladify.IntegrationTests.GraphQL.Responses;
+
+public class MutationsResponses
+{
+    public record UpdateNotificationSettingsMutationResponse(
+    [property: JsonPropertyName("updateNotificationSettings")] UserNotificationSettingsModel Result
+    );
+
+    public record PatchSubscriptionMutationResponse(
+        [property: JsonPropertyName("patchSubscription")] bool Result
+    );
+}
---
+++ Vladify.IntegrationTests/GraphQL/Responses/QueryResponses.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+using Vladify.BusinessLogic.Models;
+
+namespace Vladify.IntegrationTests.GraphQL.Responses;
+
+internal class QueryResponses
+{
+    public record NotificationByIdQueryResponse(
+    [property: JsonPropertyName("notificationById")] UserNotificationSettingsModel? Result
+    );
+
+    public record NotificationsQueryResponse(
+        [property: JsonPropertyName("notifications")] List<UserNotificationSettingsModel> Result
+    );
+
+    public record EmailSubscribersQueryResponse(
+        [property: JsonPropertyName("emailSubscribers")] List<UserNotificationSettingsModel> Result
+    );
+}
