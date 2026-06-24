bd4ddff3905967454528bbe627166c81342c96bd
Vladislav Pomozov <vladuk2332@gmail.com>
2026-06-24 13:34:05 +0300

    add autoFixtureOptions

---
+++ Vladify.IntegrationTests/AutoFixtureOptions.cs
@@ -0,0 +1,21 @@
+﻿using AutoFixture;
+using MongoDB.Bson;
+using System.Net.Mail;
+using Vladify.DataAccess.Entities;
+
+namespace Vladify.IntegrationTests;
+
+public static class AutoFixtureOptions
+{
+    public static IFixture CreateFixture()
+    {
+        var fixture = new Fixture();
+
+        fixture.Customize<UserNotificationSettings>(builder => builder
+            .With(s => s.Id, () => ObjectId.GenerateNewId().ToString())
+            .With(x => x.EmailAddress, () => fixture.Create<MailAddress>().Address)
+        );
+
+        return fixture;
+    }
+}
