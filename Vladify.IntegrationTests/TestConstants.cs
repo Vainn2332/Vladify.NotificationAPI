9f2515e546a2eb8516b2541dcc0b9b38e385599d
Vladislav Pomozov <vladuk2332@gmail.com>
2026-06-24 13:44:36 +0300

    add constants

--- Vladify.IntegrationTests/TestConstants.cs
+++ Vladify.IntegrationTests/TestConstants.cs
@@ -11,4 +11,8 @@ public static class TestConstants
     public const string BaseClaimNamespace = "https://vladify.com/";
 
     public const string CustomEmailClaimName = $"{BaseClaimNamespace}email";
+
+    public const string UserNotificationSettingsCollectionName = "UserNotificationSettingsCollection";
+
+    public const string GraphQlRoute = "/graphql";
 }
