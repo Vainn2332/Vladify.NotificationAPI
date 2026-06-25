370c15b434c5cc2ddb59e1086eba403fd8f87cc2
Vladislav Pomozov <vladuk2332@gmail.com>
2026-06-25 16:31:29 +0300

    add constant

--- Vladify.IntegrationTests/Constants/TestConstants.cs
+++ Vladify.IntegrationTests/Constants/TestConstants.cs
@@ -6,7 +6,7 @@ public static class TestConstants
 
     public const string Issuer = "testIssuer";
 
-    public static readonly string testSecretKey = Environment.GetEnvironmentVariable("TEST_JWT_SECRETKEY")
+    public static readonly string TestSecretKey = Environment.GetEnvironmentVariable("TEST_JWT_SECRETKEY")
             ?? throw new ArgumentException("Failed to get test jwt secret key");
 
     public const string BaseClaimNamespace = "https://vladify.com/";
@@ -16,4 +16,7 @@ public static class TestConstants
     public const string UserNotificationSettingsCollectionName = "Notifications";
 
     public const string GraphQlRoute = "/graphql";
+
+    public const string DbName = "TestDb";
+
 }
