1d45120423a89cffc2287e705adffac0ce522cd4
Vladislav Pomozov <vladuk2332@gmail.com>
2026-06-30 12:53:55 +0300

    update class program

--- Vladify.NotificationAPI/Program.cs
+++ Vladify.NotificationAPI/Program.cs
@@ -1,42 +1,30 @@
 using Scalar.AspNetCore;
 using Vladify.NotificationAPI.Extensions;
 
-namespace Vladify.NotificationAPI;
+var builder = WebApplication.CreateBuilder(args);
 
-public class Program
+builder.Services.AddControllers();
+
+builder.Services.AddOpenApi();
+
+builder.Services.AddAppServices(builder.Configuration);
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
 {
-    protected Program() { }
-
-    private static async Task Main(string[] args)
-    {
-        var builder = WebApplication.CreateBuilder(args);
-
-        builder.Services.AddControllers();
-
-        builder.Services.AddOpenApi();
-
-        builder.Services.AddAppServices(builder.Configuration);
-
-        var app = builder.Build();
-
-        if (app.Environment.IsDevelopment())
-        {
-            app.MapOpenApi();
-            app.MapScalarApiReference();
-        }
-
-        app.UseAuthentication();
-
-        app.UseAuthorization();
-
-        app.MapGraphQL();
-
-        app.UseHttpsRedirection();
-
-        app.UseAuthorization();
-
-        app.MapControllers();
-
-        await app.RunAsync();
-    }
+    app.MapOpenApi();
+    app.MapScalarApiReference();
 }
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapGraphQL();
+
+app.UseHttpsRedirection();
+
+app.MapControllers();
+
+await app.RunAsync();
