10033b2974d6633399ad94945107cebbd25a99bf
Vladislav Pomozov <vladuk2332@gmail.com>
2026-07-01 14:29:46 +0300

    add graphQl response wrapper classes

---
+++ Vladify.IntegrationTests/GraphQl/Dtos/ClientDtos.cs
@@ -0,0 +1,6 @@
+﻿namespace Vladify.IntegrationTests.GraphQl.Dtos;
+
+public class ClientDtos
+{
+    public record PartialSubscriberDto(string Id, string EmailAddress);
+}
---
+++ Vladify.IntegrationTests/GraphQl/Responses/QueryResponses.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+using Vladify.BusinessLogic.Models;
+
+namespace Vladify.IntegrationTests.GraphQL.Responses;
+
+public class QueryResponses
+{
+    public record NotificationByIdQueryResponse(
+        [property: JsonPropertyName("notificationById")] UserNotificationSettingsModel Result
+    );
+
+    public record NotificationsQueryResponse(
+        [property: JsonPropertyName("notifications")] UserNotificationSettingsModel[] Result
+    );
+
+    public record PartialEmailSubscribersQueryResponse(
+        [property: JsonPropertyName("emailSubscribers")] PartialSubscriberDto[] Result
+    );
+}
