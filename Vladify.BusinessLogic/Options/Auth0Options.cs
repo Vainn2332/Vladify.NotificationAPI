c39a69c1f6c108358c85702dce4425d2047d30cd
Vladislav Pomozov <vladuk2332@gmail.com>
2026-07-01 13:18:36 +0300

    add auth0Options

---
+++ Vladify.BusinessLogic/Options/Auth0Options.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vladify.BusinessLogic.Options;
+
+public class Auth0Options
+{
+    public const string SectionName = "Auth0Options";
+
+    [Required]
+    public required string Domain { get; set; }
+
+    [Required]
+    public required string Audience { get; set; }
+
+    public string Authority => $"https://{Domain}";
+}
