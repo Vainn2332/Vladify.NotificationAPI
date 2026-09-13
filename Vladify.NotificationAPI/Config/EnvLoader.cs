da6f53f320d6b2dc71df7e46ac95276fadda97aa
Vladislav Pomozov <vladuk2332@gmail.com>
2026-09-13 11:51:41 +0300

    Feat/add .env.example (#18)
    * add dotnetEnv
    
    * add .env.example

---
+++ Vladify.NotificationAPI/Config/EnvLoader.cs
@@ -0,0 +1,9 @@
+﻿namespace Vladify.NotificationAPI.Config;
+
+public static class EnvLoader
+{
+    public static void LoadEnvVariables()
+    {
+        DotNetEnv.Env.TraversePath().Load();
+    }
+}
